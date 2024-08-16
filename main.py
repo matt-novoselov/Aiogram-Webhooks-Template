677bf870e3cd8c66d90f2f47daa587bf597809fc
Matt Novoselov <matveynovoselov@gmail.com>
2024-08-16 22:42:01 +0200

    Added public domain env var

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ dotenv.load_dotenv()
 
 # Load Telegram bot API token
 TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN")
+
+# Load public domain for webhooks
+PUBLIC_DOMAIN = getenv("PUBLIC_DOMAIN")
+
+print(PUBLIC_DOMAIN)
